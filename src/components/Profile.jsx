f5dde2366320870a3e7692aac7043303174df5a5
jrosado7777 <jrosado7777@gmail.com>
2025-08-13 15:15:06 -0600

    Updated Header and browser tab name

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,11 @@ function Profile() {
     <section className="profile">
       <img src="/Jason_photo_for_resume.jpg" alt="Jason Rosado" className="profile-pic" />
       <h1>Jason Rosado</h1>
-      <p>Full Stack Developer with 2.5 years of experience in modern web technologies.</p>
-      <p>Background in business and computer science, with a passion for creativity and meaningful design.</p>
-      <p>I specialize in building scalable applications using Java, Spring Boot, React, and AWS, and I love solving real-world problems with thoughtful solutions.</p>
+      <p>I am a self-taught, Full Stack Software Engineer nearly 3 years of on-the-job experience and a proven ability to deliver both frontend and backend solutions. My strengths lie in JavaScript, React, REST, and modern UI design, complemented by solid backend expertise in Java, Spring, AWS, and Microservices.</p>
+      <p>I have developed multiple applications full stack applications, some of which you can view in my portfolio: jasonleerosado.com </p>
+      <p>I have hands-on experience in containerization with Docker, DevOps and CI/CD pipelines, and database management with PostgreSQL and DynamoDB. I thrive on learning new technologies quickly and collaborating with teams.</p>
+      <p>I love people and have years of experience training, running scrum meetings, and managing projects in coding, at the bank, and in my own personal video production business. 
+I am passionate about technology and fostering team growth, and am eager to contribute over 10 years of varied experience to a positive team.</p>
     </section>
   );
 }
