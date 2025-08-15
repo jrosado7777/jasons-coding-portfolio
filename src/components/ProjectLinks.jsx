5a9fd68a1f25d707ecca9709d6918824fca0bbeb
jrosado7777 <jrosado7777@gmail.com>
2025-08-15 11:35:24 -0600

    gofastigotta link should work maybe

--- src/components/ProjectLinks.jsx
+++ src/components/ProjectLinks.jsx
@@ -7,7 +7,7 @@ function ProjectLinks() {
           <h3>Video Portfolio Site</h3>
           <p>My side business's website, Production Studio of Colorado LLC</p>
         </a>
-        <a className="project-card" href="https:gofastigotta.com" target="_blank" rel="noopener noreferrer">
+        <a className="project-card" href="https://gofastigotta.com" target="_blank" rel="noopener noreferrer">
           <h3>Music Portfolio</h3>
           <p>My music portfolio, featuring remixes and original songs.</p>
         </a>
